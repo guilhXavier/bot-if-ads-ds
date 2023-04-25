7be5e1d90ab051434ab9ffc330dbb44d0e7bfb51
guilherme.xavier <guilherme.xavier@sap.com>
2023-04-25 15:47:01 -0300

    feat: added db modelling and initial connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-const x = 'This is an index file';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('here');
+  // Connect the client
+  await prisma.$connect();
+  console.log('here1');
+  // ... you will write your Prisma Client queries here
+  await prisma.token.create({ data: { tokenCode: '100', isExpired: true } });
+
+  const tokens = await prisma.token.findMany();
+
+  console.log(tokens);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
