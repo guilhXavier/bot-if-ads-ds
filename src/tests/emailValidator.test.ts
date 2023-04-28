ea08734e338a0e5fca4c8d8d65f2bd9d9869aa5d
guilherme.xavier <guilherme.xavier@sap.com>
2023-04-28 18:18:51 -0300

    feat: added yup validation for user email

--- src/tests/emailValidator.test.ts
+++ src/tests/emailValidator.test.ts
@@ -2,27 +2,35 @@ import { describe, expect, it } from 'vitest';
 import { emailValidator } from '../validators/emailValidator';
 
 describe('Email Validator Test Suite', () => {
-  it('should validate an email if properly formatted', () => {
+  it('should validate an email if properly formatted', async () => {
     const input = 'email@academico.ifsul.edu.br';
 
-    const result = emailValidator(input);
+    const result = await emailValidator(input);
 
     expect(result).toBeTruthy();
   });
 
-  it('should fail to validate an email with the wrong domain', () => {
+  it('should fail to validate an email with the wrong domain', async () => {
     const input = 'email@edu.br';
 
-    const result = emailValidator(input);
+    const result = await emailValidator(input);
 
     expect(result).toBeFalsy();
   });
 
-  it('should remove whitespace from an email', () => {
-    const input = 'email     @academico.ifsul.edu.br';
+  it('should fail to validate an input which is empty', async () => {
+    const input = '';
 
-    const result = emailValidator(input);
+    const result = await emailValidator(input);
 
-    expect(result).toBeTruthy();
+    expect(result).toBeFalsy();
+  });
+
+  it('should fail to validate an input which is not an email', async () => {
+    const input = 'asdasdasd';
+
+    const result = await emailValidator(input);
+
+    expect(result).toBeFalsy();
   });
 });
--- src/validators/emailValidator.ts
+++ src/validators/emailValidator.ts
@@ -1,10 +1,16 @@
-const emailValidator = (email: string): boolean => {
-  const ACADEMIC_EMAIL_DOMAIN = '@academico.ifsul.edu.br';
+import yup from 'yup';
 
-  return email
-    .toLocaleLowerCase()
-    .replace(/\s/g, '')
-    .includes(ACADEMIC_EMAIL_DOMAIN);
+const schemaEmail = yup.object().shape({
+  email: yup.string().email().required(),
+});
+
+const emailValidator = async (email: string): Promise<boolean> => {
+  const IF_EMAIL_DOMAIN = '@academico.ifsul.edu.br';
+
+  return schemaEmail
+    .validate({ email })
+    .then((value) => value.email.includes(IF_EMAIL_DOMAIN))
+    .catch(() => false);
 };
 
 export { emailValidator };
